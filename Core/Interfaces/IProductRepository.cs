5c28d6b447abffc35366552c44fc88200429ee90
Emmanuel <williamsemmanuel716@gmail.com>
2023-04-12 17:49:56 +0800

    End of section 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Core.Entities;
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+           Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
+    }
+}
